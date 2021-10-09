e21bbcc865b1673e3e1cf8e899658476946f4f23
yangshanfa <438146737@qq.com>
2021-10-09 19:50:46 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@material-ui/core/Button';
+import { createTheme, ThemeProvider, styled } from '@material-ui/core/styles';
+const theme = createTheme({
+    palette: {
+        primary: {
+            main:"#00a9eb",
+            contrastText: '#fff',
+        },
+    },
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    //fontSize: '1rem',
+                    borderRadius:0
+                },
+            },
+        },
+    },
+  /*  subtitle1: {
+        fontSize: 12,
+    },
+    body1: {
+        fontWeight: 500,
+    },*/
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"app"}>
+          <ThemeProvider theme={theme}>
+              <Button variant="contained">你好，世界</Button>
+              <div>
+                  123
+              </div>
+          </ThemeProvider>
+      </div>
+
+
   );
 }
 
